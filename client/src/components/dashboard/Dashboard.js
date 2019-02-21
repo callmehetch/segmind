758e15facd603dac497b40fb04626280aa72e297
callmehetch <callmehetch@gmail.com>
2019-02-22 01:23:59 +0530

    Push

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+import mapboxgl from 'mapbox-gl';
+mapboxgl.accessToken = 'pk.eyJ1IjoiaXRzaGVtYW50aGt1bWFyIiwiYSI6ImNqc2YwcW9tYzB5MjczeXQwOXdudmxqZ3cifQ.gOo-vHyvCp1NraKUAzmEcQ';
+
+
+
+class Dashboard extends Component {
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+
+
+ constructor(props: Props) {
+    super(props);
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 1.5
+    };
+  }
+
+  componentDidMount() {
+    const { lng, lat, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [lng, lat],
+      zoom
+    });
+
+    map.on('move', () => {
+      const { lng, lat } = map.getCenter();
+
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+
+}
+
+render() {
+  const { lng, lat, zoom } = this.state;
+  const { user } = this.props.auth;
+return (
+      <div style={{ height: "300vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12">
+            <h4>
+                <div>
+        <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+        </div>
+        <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+      </div>
+              <b>Hey there,</b> {user.name.split(" ")[0]}
+              <p className="flow-text grey-text text-darken-1">
+                You are logged into{" "}
+                <span style={{ fontFamily: "monospace" }}>SegMind</span> 👏
+              </p>
+            </h4>
+  
+            <button
+              style={{
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px",
+                marginTop: "1rem"
+              }}
+              onClick={this.onLogoutClick}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+Dashboard.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Dashboard);
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://heroku_xnpqrnq2:ptgq67vev8o1sd11dnqiq3cuuq@ds161764.mlab.com:61764/heroku_xnpqrnq2",
+  secretOrKey: "hemanth"
+};
