38b9ee1d27c54ca62a69f105fd6dd62e15c1ba93
callmehetch <callmehetch@gmail.com>
2019-02-22 23:57:57 +0530

    a

---
+++ client/build/precache-manifest.2bae5910651842e971fbdf3622401efc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fa241f24f3cfb9bf395e",
+    "url": "/static/js/main.fa241f24.chunk.js"
+  },
+  {
+    "revision": "88becf068855261fca29",
+    "url": "/static/js/2.88becf06.chunk.js"
+  },
+  {
+    "revision": "fa241f24f3cfb9bf395e",
+    "url": "/static/css/main.f1fb6444.chunk.css"
+  },
+  {
+    "revision": "f44c254a2b2004104aab78d8ac7286f1",
+    "url": "/index.html"
+  }
+];
